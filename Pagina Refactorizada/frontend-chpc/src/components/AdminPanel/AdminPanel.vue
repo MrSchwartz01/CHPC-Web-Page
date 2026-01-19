54a3a8295149d322245651749dc6e72ef4a9d5d2
MrSchwartz01 <xavierferrin01@gmail.com>
2026-01-19 09:05:46 -0500

    Ruta actualizada
    Cambio de ruta de promociones en el panel de admin

--- Pagina Refactorizada/frontend-chpc/src/components/AdminPanel/AdminPanel.vue
+++ Pagina Refactorizada/frontend-chpc/src/components/AdminPanel/AdminPanel.vue
@@ -796,7 +796,7 @@ export default {
 
     async loadProductos() {
       try {
-        const response = await axios.get(`${API_BASE_URL}/productos`);
+        const response = await axios.get(`${API_BASE_URL}/tienda/productos`);
         this.productos = response.data;
       } catch (error) {
         console.error('Error al cargar productos:', error);
